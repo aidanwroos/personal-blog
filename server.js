269bd7db5d397503ea1804017f051f9159aa8038
Aidan Roos <aidanwroos@gmail.com>
2022-09-11 21:17:43 -0700

    commit on index.html

--- server.js
+++ server.js
@@ -2,8 +2,44 @@ var PORT = 8000;
 
 var http = require('http');
 
-var server = http.createServer(requestHandler)
+const fs = require('fs');
+
+const path = './public/blog-data.json';
+
+//var blog_json_file = require('./public/blog-data.json');
+
+const Person = {
+    "name" : "Aian",
+    "age" : 1,
+    "sex" : "mal"
+};
+
+const data = JSON.stringify(Person);
 
 
+fs.readFile(path, 'utf-8', (err, jsonString) => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        const json_data = JSON.parse(jsonString);
+        console.log(json_data.name);
+        console.log(json_data.age);
+        console.log(json_data.sex);
 
-server.listen(PORT);
+        fs.writeFile(path, data, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON data is saved.");
+        }) 
+    } 
+});
+
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url);
+    res.end('Hello World!');
+    
+}).listen(PORT);
